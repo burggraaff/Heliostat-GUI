a1e2a2a73af9fb38268a23d3f35937c52d1b73c7
Olivier Burggraaff <olivierburggraaff@gmail.com>
2018-01-03 16:52:11 +0100

    Changed visitor GUI to copy of student

--- visitor.py
+++ visitor.py
@@ -1,21 +1,21 @@
-import gui as G
+import gui
 
-class Page1(G.Page):
+class Page1(gui.Page):
     def __init__(self, parent, controller):
-        G.Page.__init__(self, parent, controller, langfile = "visitor1_lang", nextpage = Page2)
-        self.title = G.tk.Label(self, text = "VISITOR PAGE 1", font = G.LABEL_FONT)
+        gui.Page.__init__(self, parent, controller, langfile = "visitor_lang")
+        
+        self.title = gui.tk.Label(self, text = "VISITOR SCREEN", font = gui.LABEL_FONT)
         self.title.grid(row = 0, column = 1, columnspan = 10, stick = "news")
         
-        self.sun = G.Sun(parent = self, width = 600, height = 400, padx=25, pady=10, background="#000000")
-        self.sun.grid(row = 1, column = 1)
+        self.controls = gui.Controls(parent = self, controller = self.controller)
+        self.controls.grid(row = 1, column = 1, sticky = "n", padx = 25)
         
-class Page2(G.Page):
-    def __init__(self, parent, controller):
-        G.Page.__init__(self, parent, controller, langfile = "visitor2_lang", previous = Page1, nextpage = Page1)
-        self.title = G.tk.Label(self, text = "VISITOR PAGE 2", font = G.LABEL_FONT)
-        self.title.grid(row = 0, column = 1, columnspan = 10, stick = "news")
+        self.spectrum = gui.Spectrum(parent = self, controller = self.controller)
+        self.spectrum.grid(row = 1, column = 2, sticky = "n", padx = 25)
         
-pages = [Page1, Page2]
+        self.translatables = [self.title, self.controls.sun.button, self.controls.align.button, self.spectrum.button, self.spectrum.label_integration]
 
-GUI = G.Root(pages)
+pages = [Page1]
+
+GUI = gui.Root(pages)
 GUI.mainloop()
