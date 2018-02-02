df71a941229bc56df1826747890ccf900fd93078
Olivier Burggraaff <olivierburggraaff@gmail.com>
2018-02-02 12:58:20 +0100

    X, Y plots

--- fit_fibre.py
+++ fit_fibre.py
@@ -46,29 +46,45 @@ class FitFrame(tk.Frame):
         self.aligner = Aligner()
 
         self.b_inte = tk.Button(self, text = "UPDATE", font = LABEL_FONT, height = 2, width = 10, command = self.update)
-        self.b_inte.grid(row = 0, column = 2, sticky = "news")
+        self.b_inte.grid(row = 0, column = 4, sticky = "news")
         self.b_plot = tk.Button(self, text = "PHOTO", font = LABEL_FONT, height = 2, width = 10, command = self.photo)
-        self.b_plot.grid(row = 1, column = 2, sticky = "news")
+        self.b_plot.grid(row = 1, column = 4, sticky = "news")
         self.b_find = tk.Button(self, text = "FIND LEDS", font = LABEL_FONT, height = 2, width = 10, command = self.find_leds)
-        self.b_find.grid(row = 2, column = 2, sticky = "news")
+        self.b_find.grid(row = 2, column = 4, sticky = "news")
         self.b_opti = tk.Button(self, text = "OPTIMISE", font = LABEL_FONT, height = 2, width = 10, command = self.optimise)
-        self.b_opti.grid(row = 3, column = 2, sticky = "news")
+        self.b_opti.grid(row = 3, column = 4, sticky = "news")
         self.b_save = tk.Button(self, text = "SAVE", font = LABEL_FONT, height = 2, width = 10, command = self.save)
-        self.b_save.grid(row = 4, column = 2, sticky = "news")
+        self.b_save.grid(row = 4, column = 4, sticky = "news")
 
-        self.fig = plt.figure(figsize=(10,10), facecolor = "none")
+        self.fig = plt.figure(figsize=(7,5), facecolor = "none", tight_layout=True)
         self.canvas = FigureCanvasTkAgg(self.fig, self)
-        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=100)
+        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=2)
+
+        self.fig_xx = plt.figure(figsize=(4,4), facecolor = "none", tight_layout=True)
+        self.canvas_xx = FigureCanvasTkAgg(self.fig_xx, self)
+        self.canvas_xx.get_tk_widget().grid(row=0, column=1, rowspan=2)
+
+        self.fig_yy = plt.figure(figsize=(4,4), facecolor = "none", tight_layout=True)
+        self.canvas_yy = FigureCanvasTkAgg(self.fig_yy, self)
+        self.canvas_yy.get_tk_widget().grid(row=3, column=1, rowspan=2)
+
+        self.fig_x = plt.figure(figsize=(4,4), facecolor="none", tight_layout=True)
+        self.canvas_x = FigureCanvasTkAgg(self.fig_x, self)
+        self.canvas_x.get_tk_widget().grid(row=0, column=2, rowspan=2)
+
+        self.fig_y = plt.figure(figsize=(4,4), facecolor="none", tight_layout=True)
+        self.canvas_y = FigureCanvasTkAgg(self.fig_y, self)
+        self.canvas_y.get_tk_widget().grid(row=3, column=2, rowspan=2)
 
         self.f = "{0}: {1:+.4f}" # format to print values in
         self.indicator_x = tk.Label(self, text = self.f.format("X", self.x), font = LABEL_FONT)
-        self.indicator_x.grid(row = 0, column = 1, sticky = "w")
+        self.indicator_x.grid(row = 0, column = 3, sticky = "w")
         self.indicator_y = tk.Label(self, text = self.f.format("Y", self.y), font = LABEL_FONT)
-        self.indicator_y.grid(row = 1, column = 1, sticky = "w")
+        self.indicator_y.grid(row = 1, column = 3, sticky = "w")
         self.indicator_f = tk.Label(self, text = "F: (-1, -1)", font = LABEL_FONT)
-        self.indicator_f.grid(row = 2, column = 1, sticky = "w")
+        self.indicator_f.grid(row = 2, column = 3, sticky = "w")
         self.indicator_l = tk.Label(self, text = "L: 00000", font = LABEL_FONT)
-        self.indicator_l.grid(row = 3, column = 1, sticky = "w")
+        self.indicator_l.grid(row = 3, column = 3, sticky = "w")
 
     def end(self):
         if self.aligner is not None:
@@ -103,6 +119,21 @@ class FitFrame(tk.Frame):
         table = np.vstack((table, new_row))
         np.savetxt("static/fibre_positions.txt", table, fmt="%.4f")
         print("Saved!")
+        for i, f in enumerate([self.fig_x, self.fig_y]):
+            ax = f.gca()
+            ax.clear()
+            ax.scatter(table[:,0], table[:,1], c=table[:,i+2], vmin=-0.15, vmax=1.8, s=35)
+            ax.set_xlim(0, 1280)
+            ax.set_ylim(1024, 0)
+            f.canvas.draw()
+        for i, f in enumerate([self.fig_xx, self.fig_yy]):
+            ax = f.gca()
+            ax.clear()
+            ax.scatter(table[:,i], table[:,i+2])
+            ax.set_xlim(0, 1024 + i*256)
+            ax.set_ylim(-0.15, 1.8)
+            f.canvas.draw()
+        print("Plotted!")
 
     def plot(self):
         leds = self.aligner.led_coords
